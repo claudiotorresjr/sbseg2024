dfcae85d3143f8412a0939720c9a656951fe2893
Claudio Torres Junior <claudiotorresjunior@gmail.com>
2024-09-15 13:23:36 -0300

    asad

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-console.log("Exploiting the server!");
+const fs = require('fs');
+const path = require('path');
 
+// Diretório que você deseja listar
+const directoryPath = "/app";
+
+// Função para listar o conteúdo do diretório
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        return console.error('Erro ao listar o diretório:', err);
+    }
+    
+    console.log('Conteúdo do diretório:');
+    files.forEach(file => {
+        console.log(file);
+    });
+});
